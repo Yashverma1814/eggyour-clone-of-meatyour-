211660110cdabb94663f977f191ff4ad7d4b40f3
Yash Verma <yashv1752@gmail.com>
2022-11-24 11:18:03 +0530

    offer nav completed

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { DeliveryNav } from '../components/DeliveryNav'
+
+export const HomePage = () => {
+  return (
+    <div>
+        <DeliveryNav />
+    </div>
+  )
+}
---
+++ src/components/DeliveryNav.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const DeliveryNav = () => {
+  return (
+    <div className='delNav'>Delivery only in -Delhi - NCR , Pune , Mumbai , Thane , Navi mumbai , Panvel</div>
+  )
+}
